d36ecc36391f2a3dfd12ba3493d8e423ea58f5d3
Boris Bovkunovich <yakov.antipov.94@mail.ru>
2025-07-24 12:28:01 +0300

    adaptive 480

---
+++ callback.js
@@ -0,0 +1,14 @@
+const backToTopButton = document.querySelector(".callback");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.classList.add("visible");
+  } else {
+    backToTopButton.classList.remove("visible");
+  }
+});
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
